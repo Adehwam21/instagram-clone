bc8b5551dfceeb257cef85741da669ba25b757de
Adehwam21 <100027234+Adehwam21@users.noreply.github.com>
2023-04-02 23:32:25 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Homepage from './component/homepage/Homepage';
+
+function App() {
+  return (
+   <div className='app'><Homepage/></div>
+  );
+}
+
+export default App;
