454c074d6cfd6c0e5f0ebc4eb12985e99581d557
Adehwam21 <100027234+Adehwam21@users.noreply.github.com>
2023-04-03 16:24:12 +0000

    added new features

--- src/timeline/post/Posts.js
+++ src/timeline/post/Posts.js
@@ -1,21 +1,45 @@
 import React from 'react'
+import "./Posts.css";
 import {Avatar} from '@mui/material'
-import MoreHorizIcon from '@mui/icons-material'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+// import "./timeline/Timeline.js"
+import * as TbIcons from 'react-icons/tb'
+import * as BsIcons from 'react-icons/bs'
+import * as BiIcons from 'react-icons/bi'
 
-function Posts() {
+function Posts({user, postContent, likes, timestamp}) {
   return (
     <div className='post'>
         <div className='post-header'>
             <div className='post-header-author'>
-                <Avatar>A</Avatar>
-                aaron__•<span>9h</span>
+                <Avatar sx={{marginRight: 1}}>{user.charAt(0).toUpperCase()}</Avatar>
+                {user}.{" "} • <span>{timestamp}</span>
+            </div>
+            <MoreHorizIcon/>
+        </div>
+
+        <div className='post-image'>
+          <img src={postContent} alt='phpto'/>
+        </div>
+
+        <div className='post-footer'>
+
+          <div className='post-footer-icon'>
+
+            <div className='post-icon-main'>
+              <FavoriteBorderIcon className='post-icon' sx={{fontSize: 25}} />
+              <TbIcons.TbMessageCircle2 className='post-icon' size="1.55rem" />
+              <BsIcons.BsSend className='post-icon' size="1.3rem"/>
             </div>
 
-            <MoreHorizIcon/>
-            
+            <div className='post-icon-save'>
+              <BiIcons.BiBookmark className='post-icon' size="1.3rem"/>
+            </div>
+
+          </div>
+          <p>Liked by {likes} others</p>
         </div>
-        <div className='post-image'></div>
-        <div className='post-footer'></div>
     </div>
   )
 }
